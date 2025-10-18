281ba7dfc12ca1fe4226ef4a842c1a489687ab49
reElcy <reElcy@yandex.ru>
2025-10-18 17:16:47 +0300

    feat: add get user

--- internal/repository/user.go
+++ internal/repository/user.go
@@ -2,9 +2,13 @@ package repository
 
 import (
 	"context"
+	"errors"
 	"hackathon-back/internal/apperrors"
+	"hackathon-back/internal/model"
 
 	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgconn"
 	"github.com/jackc/pgx/v5/pgxpool"
 
 	_ "hackathon-back/internal/model"
@@ -20,6 +24,117 @@ func NewUserRepository(db *pgxpool.Pool) *UserRepository {
 
 func (r *UserRepository) Pool() *pgxpool.Pool {
 	return r.db
+}
+
+func (r *UserRepository) InsertUser(ctx context.Context, ext RepoExtension, user *model.User) (*model.User, error) {
+	if ext == nil {
+		ext = r.db
+	}
+
+	const query = `
+		INSERT INTO sso.users (id, username, email, password)
+		VALUES ($1, $2, $3, $4)
+		RETURNING confirmed, deleted, blocked, role, created_at, updated_at;
+	`
+
+	err := ext.QueryRow(ctx, query,
+		user.ID,
+		user.Username,
+		user.Email,
+		user.HashedPassword,
+	).Scan(
+		&user.Confirmed,
+		&user.Deleted,
+		&user.Blocked,
+		&user.Role,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	)
+	if err != nil {
+		var pgErr *pgconn.PgError
+
+		if errors.As(err, &pgErr) && pgErr.Code == "23505" {
+			return nil, apperrors.ErrUserAlreadyExists
+		}
+
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func (r *UserRepository) SelectUserByID(ctx context.Context, ext RepoExtension, id uuid.UUID) (*model.User, error) {
+	if ext == nil {
+		ext = r.db
+	}
+
+	const query = `
+		SELECT id, username, email, password, confirmed, deleted, blocked, role, created_at, updated_at
+		FROM sso.users
+		WHERE id = $1
+		  AND deleted = false 
+		  AND blocked = false;
+	`
+
+	var user model.User
+
+	if err := ext.QueryRow(ctx, query, id).Scan(
+		&user.ID,
+		&user.Username,
+		&user.Email,
+		&user.HashedPassword,
+		&user.Confirmed,
+		&user.Deleted,
+		&user.Blocked,
+		&user.Role,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	); err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, apperrors.ErrUserDoesNotExist
+		}
+
+		return nil, err
+	}
+
+	return &user, nil
+}
+
+func (r *UserRepository) SelectUserByEmail(ctx context.Context, ext RepoExtension, email string) (*model.User, error) {
+	if ext == nil {
+		ext = r.db
+	}
+
+	const query = `
+		SELECT id, username, email, password, confirmed, deleted, blocked, role, created_at, updated_at
+		FROM sso.users
+		WHERE email = $1
+		  AND deleted = false 
+		  AND blocked = false;
+	`
+
+	var user model.User
+
+	if err := ext.QueryRow(ctx, query, email).Scan(
+		&user.ID,
+		&user.Username,
+		&user.Email,
+		&user.HashedPassword,
+		&user.Confirmed,
+		&user.Deleted,
+		&user.Blocked,
+		&user.Role,
+		&user.CreatedAt,
+		&user.UpdatedAt,
+	); err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, apperrors.ErrUserDoesNotExist
+		}
+
+		return nil, err
+	}
+
+	return &user, nil
 }
 
 func (r *UserRepository) Delete(ctx context.Context, ext RepoExtension, id uuid.UUID) error {

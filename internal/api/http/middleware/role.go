4ac776c0c7f9f2054707f6910b7216e78c246791
reElcy <reElcy@yandex.ru>
2025-10-18 12:41:02 +0300

    refactor: add role middleware fixes

--- internal/api/http/middleware/role.go
+++ internal/api/http/middleware/role.go
@@ -1,25 +1,31 @@
 package middleware
 
 import (
-	"github.com/gin-gonic/gin"
 	"hackathon-back/internal/api/http/handler"
 	"hackathon-back/internal/model"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func RequireRoles(allowedRoles ...string) gin.HandlerFunc {
+	roleSet := make(map[string]struct{}, len(allowedRoles))
+
+	for _, role := range allowedRoles {
+		roleSet[role] = struct{}{}
+	}
+
 	return func(c *gin.Context) {
-		// берем роль из контекста
 		roleVal, exists := c.Get(model.UserRoleKey)
 		if !exists {
 			c.AbortWithStatusJSON(http.StatusForbidden, handler.ResponseWithMessage{
 				Status:  handler.StatusNotPermitted,
-				Message: "user role not found in context",
+				Message: "no data about the users roles",
 			})
+
 			return
 		}
 
-		// валидируем ее
 		role, ok := roleVal.(string)
 		if !ok {
 			c.AbortWithStatusJSON(http.StatusForbidden, handler.ResponseWithMessage{
@@ -28,18 +34,15 @@ func RequireRoles(allowedRoles ...string) gin.HandlerFunc {
 			})
 		}
 
-		// проверяем, есть ли роль среди разрешенных
-		for _, allowedRole := range allowedRoles {
-			if role == allowedRole {
-				c.Next()
-				return
-			}
+		if _, ok := roleSet[role]; ok {
+			c.Next()
+
+			return
 		}
 
-		// если не одна не подошла - отказ
 		c.AbortWithStatusJSON(http.StatusForbidden, handler.ResponseWithMessage{
-			Status:  handler.StatusNotPermitted,
-			Message: "access denied: insufficient permissions",
+			Status:  handler.StatusForbidden,
+			Message: "insufficient roles privileges",
 		})
 	}
 }

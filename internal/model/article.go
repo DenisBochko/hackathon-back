1d9d6fe2da93cefa6c05c46bab2a73e639149ba8
reElcy <reElcy@yandex.ru>
2025-10-23 02:40:37 +0300

    feat: add ArticleCreateRequest

--- internal/model/article.go
+++ internal/model/article.go
@@ -6,6 +6,13 @@ import (
 	"github.com/google/uuid"
 )
 
+type ArticleCreateRequest struct {
+	TitleRU   string `json:"title_ru,omitempty"`
+	TitleEN   string `json:"title_en,omitempty"`
+	ContentRU string `json:"content_ru,omitempty"`
+	ContentEN string `json:"content_en,omitempty"`
+}
+
 type Article struct {
 	ID        uuid.UUID `json:"id"`
 	TitleRU   string    `json:"title_ru,omitempty"`
@@ -14,4 +21,9 @@ type Article struct {
 	ContentEN string    `json:"content_en,omitempty"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type SearchResult struct {
+	Article   Article             `json:"article"`
+	Highlight map[string][]string `json:"highlight,omitempty"`
 }

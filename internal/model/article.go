92b693538a9a76db4b7f8f24dbde873d9081350a
reElcy <reElcy@yandex.ru>
2025-10-23 01:23:40 +0300

    feat: add article model

---
+++ internal/model/article.go
@@ -0,0 +1,17 @@
+package model
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type Article struct {
+	ID        uuid.UUID `json:"id"`
+	TitleRU   string    `json:"title_ru,omitempty"`
+	TitleEN   string    `json:"title_en,omitempty"`
+	ContentRU string    `json:"content_ru,omitempty"`
+	ContentEN string    `json:"content_en,omitempty"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
